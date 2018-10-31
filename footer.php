2061537f86c21a4915b9909239d61cb83a59ebfc
saowaneeare <44454590+saowaneeare@users.noreply.github.com>
2018-10-31 22:24:46 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer class="main-footer">
+		<div class="container">
+			<div class="f-left">
+			<p>&copy; 2018 - Advaced WP Theme </p>
+			</div>
+
+			<div class="f-right">
+				<ul>
+					<li><a href="index.html">Home</a></li>
+					<li><a href="about.html">About</a></li>
+					<li><a href="#">Services</a></li>
+				</ul>
+			</div> 
+
+		</div> 
+	</footer> 
+	<?php wp_footer(); ?>
+</body
+</html>
